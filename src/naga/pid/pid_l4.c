caa8960d4411b9ba05a187109b6f9d8d9519070c
Jonanthan <415653903@qq.com>
2015-09-05 18:56:22 +0800

    pid_l4.c

--- src/naga/pid/pid_l4.c
+++ src/naga/pid/pid_l4.c
@@ -8,7 +8,7 @@ berr pid_L4(struct pbuf *p, hytag_t *hytag,  uint8_t protocol, int outer_inner)
 	switch(protocol)
 	{
         case IP_PROTO_UDPLITE:
-		case IP_PROTO_UDP
+		case IP_PROTO_UDP:
 			return pid_udp(p,  hytag, outer_inner);
 			break;
 
