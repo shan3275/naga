73df6f5dde42166d595dda949a597621d653167c
willardalong <772413253@qq.com>
2015-10-20 22:09:54 +0800

    提交移动的开关

--- src/naga/url/url.c
+++ src/naga/url/url.c
@@ -23,13 +23,13 @@ berr  naga_uri(hytag_t *hytag)
     char *tailptr = NULL;
 
 
-    //if(hytag->uri_len >= 8 )    
-    //    tailptr = (char *)(&(hytag->uri[hytag->uri_len - 6]));//the last 
-    //else
-     tailptr = (char *)(hytag->uri);
+    if(hytag->uri_len >= 8)    
+        tailptr = (char *)(&(hytag->uri[hytag->uri_len - 4]));//the last 
+    else
+        tailptr = (char *)(hytag->uri);
     
-    if(strstr(tailptr,  tail))
-    //if(!strcmp(tailptr, tail))
+    //if(strstr(tailptr,  tail))
+    if(!strcmp(tailptr, tail))
     {
          CNT_INC(ADP_PUSH_ASSERT);
          hytag->pushed_second_assert = 1;            
