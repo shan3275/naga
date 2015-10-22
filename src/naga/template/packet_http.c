ce51e0799143c58ee6e2b25b1aec2ff589c69b1a
willardalong <772413253@qq.com>
2015-10-22 11:50:08 +0800

    s

--- src/naga/template/packet_http.c
+++ src/naga/template/packet_http.c
@@ -585,12 +585,23 @@ berr ads_http_ok_head_fill(char *buf, hytag_t *hytag)
     /* get content length */
     content_len = http_content_len_get(hytag);
     hytag->content_len = content_len;
-
-    len += sprintf(buf+len, "%s%s%s%d%s%s%s", 
+#if 0
+    len += snprintf(buf+len, "%s%s%s%d%s%s%s", 
         http_head_response1, http_head_response2, 
         http_head_response3, content_len, http_head_response5,
         http_head_response6, http_head_response7);
-    
+
+#else
+        len += snprintf(buf+len, 2048-len, "%s", http_head_response1);
+        len += snprintf(buf+len, 2048-len, "%s", http_head_response2);
+        len += snprintf(buf+len, 2048-len, "%s",http_head_response3);
+        len += snprintf(buf+len, 2048-len, "%d", content_len);
+        len += snprintf(buf+len, 2048-len, "%s",http_head_response5);
+        len += snprintf(buf+len, 2048-len, "%s", http_head_response6); 
+        len += snprintf(buf+len, 2048-len, "%s",http_head_response7); 
+#endif
+        
+       
 /*
     len += snprintf(buf+len, 2048-len, "%s%s%s%d%s%s%s", 
         http_head_response1, http_head_response2, 
