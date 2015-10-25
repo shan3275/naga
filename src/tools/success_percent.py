fcfbda9bcccc1af643e525d5e94e8509066c7247
willardalong <772413253@qq.com>
2015-10-26 02:01:52 +0800

    new

--- src/tools/success_percent.py
+++ src/tools/success_percent.py
@@ -31,6 +31,7 @@ LOG_FILE=open(ARGV1)
 for LINE in LOG_FILE.readlines():
     LINE=LINE.strip('\n')
     FIELD  = LINE.split('|')
+    print FIELD[5]
     SNET   = FIELD[5]
     DOMAIN = FIELD[6]
     PUSH   = FIELD[7]
