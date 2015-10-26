399e3741ee203fedbf08aa7fe51cc7a772024610
willardalong <772413253@qq.com>
2015-10-26 23:36:13 +0800

    s

--- src/naga/url/url.c
+++ src/naga/url/url.c
@@ -142,9 +142,11 @@ berr  naga_uri(hytag_t *hytag)
 
                 if(compare > 0)
                 {
-                   ACL_HIT(urlcre->acl);
-                   HYTAG_ACL_MERGE(hytag->acl, urlcre->acl);
-                   return E_SUCCESS;
+                   //ACL_HIT(urlcre->acl);
+                    HYTAG_ACL_MERGE(hytag->acl, urlcre->acl);
+				  	printf("action = 0x%x\n", urlcre->acl.actions);
+				   
+                   	return E_SUCCESS;
                 }
                 
 
