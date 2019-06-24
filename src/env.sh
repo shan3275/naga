a60b714ef155b90f30a5f136c44fd8cde41b68cd
“Cooper <shan3275@gmail.com>
2019-06-24 16:14:11 +0800

    编译库代码完成，下一步链接编译可执行文件

--- src/env.sh
+++ src/env.sh
@@ -2,7 +2,7 @@ export RTE_SDK=`pwd`/libdpdk/dpdk
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 export DESTDIR=`pwd`/libdpdk/dpdk
 make -C libdpdk
-make -C naga/pcre-m/
-make -C naga/http_parser/
-make -C naga/libzmq/
-make -C naga/libevent/
+make -C pcre-m
+make -C http_parser
+make -C libzmq
+make -C libevent
