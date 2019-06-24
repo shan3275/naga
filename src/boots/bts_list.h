83633e3c959a59abefdeaad0ceab9bb2d6381310
德山 刘 <shan@centos-linux.shared>
2019-06-24 14:35:20 +0800

    修改编译产生的警告

--- src/boots/bts_list.h
+++ src/boots/bts_list.h
@@ -2,11 +2,11 @@
 #define __LIST_H__
 
 #include <stddef.h>
-
+/*
 #define container_of(ptr, type, member) ({\
         const typeof( ((type *)0)->member ) *__mptr = (ptr);\
         (type *)( (char *)__mptr - offsetof(type,member) );})
-
+*/
 /*
  * Simple doubly linked list implementation.
  *
